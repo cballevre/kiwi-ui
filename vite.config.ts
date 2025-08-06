869c74096165ccbde011110907bb7480082fb210
cballevre <celestin.ballevre@protonmail.com>
2025-08-06 10:31:46 +0200

    feat: Add Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
+import { resolve } from 'node:path';
 import { peerDependencies } from './package.json';
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts(), tailwindcss()],
   build: {
     target: 'esnext',
     minify: false,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'index',
+      cssFileName: 'style',
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
