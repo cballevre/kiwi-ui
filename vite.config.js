83966a9ef8e8a51eab791c50b85e65c20672fc8e
cballevre <celestin.ballevre@protonmail.com>
2025-08-06 10:21:52 +0200

    feat: Add React

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 import { resolve } from 'node:path';
+import { peerDependencies } from './package.json';
 
 export default defineConfig({
-  plugins: [],
+  plugins: [react()],
   build: {
     target: 'esnext',
     minify: false,
@@ -11,6 +13,10 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.js'),
       fileName: 'index',
       formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // Exclude peer dependencies from the bundle to reduce bundle size
+      external: ['react/jsx-runtime', ...Object.keys(peerDependencies)],
     },
   },
 });
