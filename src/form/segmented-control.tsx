0dd6310ce756baf5c3d446fe165c03aafa1a161d
cballevre <celestin.ballevre@protonmail.com>
2025-08-07 15:56:33 +0200

    feat: Use forwardRef instead FC

--- src/form/segmented-control.tsx
+++ src/form/segmented-control.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { type FC, useState } from 'react';
+import { forwardRef, useState } from 'react';
 import {
   type Key,
   ToggleButton,
@@ -12,42 +12,41 @@ interface SegmentedControlProps extends InputWrapperProps {
   options?: string[];
 }
 
-const SegmentedControl: FC<SegmentedControlProps> = ({
-  label,
-  description,
-  options,
-}) => {
-  const [value, setValue] = useState<Set<Key> | undefined>(
-    options && options[1] !== undefined ? new Set([options[1]]) : undefined,
-  );
+const SegmentedControl = forwardRef<HTMLDivElement, SegmentedControlProps>(
+  ({ label, description, options }, ref) => {
+    const [value, setValue] = useState<Set<Key> | undefined>(
+      options && options[1] !== undefined ? new Set([options[1]]) : undefined,
+    );
 
-  const handleSelectionChange = (selectedKeys: Set<Key>) => {
-    setValue(selectedKeys);
-  };
+    const handleSelectionChange = (selectedKeys: Set<Key>) => {
+      setValue(selectedKeys);
+    };
 
-  return (
-    <InputWrapper label={label} description={description}>
-      <ToggleButtonGroup
-        className="tabs tabs-box"
-        selectionMode="single"
-        disallowEmptySelection
-        selectedKeys={value}
-        onSelectionChange={handleSelectionChange}
-      >
-        {options?.map((option) => (
-          <ToggleButton
-            key={option}
-            id={option}
-            className={({ isSelected }) =>
-              clsx('tab', isSelected && 'tab-active')
-            }
-          >
-            {option}
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup>
-    </InputWrapper>
-  );
-};
+    return (
+      <InputWrapper label={label} description={description}>
+        <ToggleButtonGroup
+          ref={ref}
+          className="tabs tabs-box"
+          selectionMode="single"
+          disallowEmptySelection
+          selectedKeys={value}
+          onSelectionChange={handleSelectionChange}
+        >
+          {options?.map((option) => (
+            <ToggleButton
+              key={option}
+              id={option}
+              className={({ isSelected }) =>
+                clsx('tab', isSelected && 'tab-active')
+              }
+            >
+              {option}
+            </ToggleButton>
+          ))}
+        </ToggleButtonGroup>
+      </InputWrapper>
+    );
+  },
+);
 
 export { SegmentedControl };
